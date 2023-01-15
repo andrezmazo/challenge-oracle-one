659c4676fcd3eec031b2445b896bf456a17fa3bc
andrez.mazo@gmail.com <andrez.mazo@gmail.com>
2023-01-15 15:33:42 -0500

    Encrypt done

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -2,15 +2,21 @@
 import "./Result.css";
 import person from "../../assets/person.png";
 
-function Result() {
+function Result(props) {
   return (
     <div className="box-result">
-      <div className="Result">
+      <div className="Result" style={{ display: props.isShown ? "" : "none" }}>
         <div className="box-image">
           <img src={person} className="person-img" alt="logo" />
         </div>
         <p>Ningún mensaje fue encontrado</p>
         <p>Ingresa el texto que desees encriptar o desencriptar.</p>
+      </div>
+      <div
+        className="text-result"
+        style={{ display: props.isShown ? "none" : "" }}
+      > <h2>{props.setTitle}</h2>
+        <h3>{props.setPropText}</h3>
       </div>
     </div>
   );
