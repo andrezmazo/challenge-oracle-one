1960580d424809346488f90c72c4f11844fada5d
andrez.mazo@gmail.com <andrez.mazo@gmail.com>
2023-01-15 10:02:17 -0500

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,30 @@
-import logo from '../assets/logo.png';
-import exclamation from '../assets/exclamation.png';
-import './App.css';
-import Result from './Result/Result.jsx'
+import logo from "../assets/logo.png";
+import exclamation from "../assets/exclamation.png";
+import "./App.css";
+import Result from "./Result/Result.jsx";
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <div className='box-main'>
-        <div className='box-input'>
-          <input type='text' className='input-text' value='ingrese texto aquí' >
-          </input>
-          <div className='box-warning'>
-            <img src={exclamation} alt="" srcset="" className='exclamation'/>
+      <div className="box-main">
+        <div className="box-input">
+          <input
+            type="text"
+            className="input-text"
+            value="ingrese texto aquí"
+          ></input>
+          <div className="box-warning">
+            <img src={exclamation} alt="" srcset="" className="exclamation" />
             <p>Solo letras minúsculas y sin acentos</p>
           </div>
-          <div className='box-button'>
-          <button type='button' className='primary-button' >Encriptar</button>
-          <button className='secondary-button'>Desencriptar</button>
-
+          <div className="box-button">
+            <button type="button" className="primary-button">
+              Encriptar
+            </button>
+            <button type="button" className="secondary-button">
+              Desencriptar
+            </button>
           </div>
         </div>
         <Result />
